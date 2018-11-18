09225d9db65e28156ad8449965980a15c3d7819f
Alfredo Morgen <alfredo7romero@gmail.com>
2018-11-18 16:13:44 +0700

    [FEATURE] Added new Webpack plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
   entry: './src/index.js',
   output: {
     filename: 'main.js',
@@ -14,5 +18,21 @@ module.exports = {
         use: ['babel-loader'],
       }
     ]
-  }
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      inject: false,
+      template: require('html-webpack-template'),
+      mobile: true,
+      meta: [
+        {
+          name: 'viewport',
+          content: 'width=device-width, initial-scale=1',
+        },
+      ],
+      title: 'Tax Calc',
+      bodyHtmlSnippet: '<div id="app"></div>',
+    }),
+  ],
 };
