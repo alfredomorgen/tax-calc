1cd9104b650aec3dc1abe261e081db98be6685db
Alfredo Morgen <alfredo7romero@gmail.com>
2018-11-21 00:28:04 +0700

    [FEATURE] Added CSS Webpack Loader and Webpack Dev Server

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,30 @@
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
+const Path = require('path');
 
 module.exports = {
   mode: 'development',
   devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+  },
   entry: './src/index.js',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
+    path: Path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
       {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
         test: /\.js$/,
         exclude: /node_modules/,
         use: ['babel-loader'],
-      }
-    ]
+      },
+    ],
   },
   plugins: [
     new CleanWebpackPlugin(['dist']),
