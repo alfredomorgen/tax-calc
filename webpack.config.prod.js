04499b2cabdb790a82ae26203525ab68aeee96b6
Alfredo Morgen <alfredo7romero@gmail.com>
2019-02-21 04:07:24 +0700

    [FEATURE] Separate webpack config for dev and prod

---
+++ webpack.config.prod.js
@@ -0,0 +1,41 @@
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: Path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      inject: false,
+      template: require('html-webpack-template'),
+      mobile: true,
+      meta: [
+        {
+          name: 'viewport',
+          content: 'width=device-width, initial-scale=1',
+        },
+      ],
+      title: 'Tax Calc',
+      bodyHtmlSnippet: '<div id="app"></div>',
+    }),
+  ],
+};
